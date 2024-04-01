9baf5313bf185da6aea14ba83a91e3ed54eec3ce
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-04-01 04:41:17 -0300

    feat: exercicios 4 e 5 concluidos semana 10

--- src/components/CardTrilha/index.jsx
+++ src/components/CardTrilha/index.jsx
@@ -1,10 +1,28 @@
-function CardTrilha() {
+import * as PropTypes from "prop-types";
+
+function CardTrilha({ dadosTrilha }) {
 
     return (
         <>
-            <h1>Card Trilha</h1>
+            <h1>{dadosTrilha.nomeTrilha}</h1>
+            <span>{dadosTrilha.cidade} / {dadosTrilha.estado}</span>
+            <img width={200} src={dadosTrilha.urlImagem} alt="imagem trilha" />
         </>
     );
+}
+
+CardTrilha.propTypes = {
+    dadosTrilha: PropTypes.exact({
+        nomeTrilha: PropTypes.string.isRequired,
+        cidade: PropTypes.string.isRequired,
+        estado: PropTypes.string.isRequired,
+        duracao: PropTypes.number.isRequired,
+        trajeto: PropTypes.number.isRequired,
+        dificuldade: PropTypes.string.isRequired,
+        tipo: PropTypes.oneOf(["caminhada / trekking", "ciclismo"]),
+        nomeUsuario: PropTypes.string.isRequired,
+        urlImagem: PropTypes.string.isRequired
+    })
 }
 
 export default CardTrilha;
