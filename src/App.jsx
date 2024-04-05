7c67cdfe094258df415e61683b5b23a4fa48016f
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-04-05 14:34:13 -0300

    feat: Exercicio 01 criar JSON de trilhas

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,7 @@ import "./App.css";
 function App() {
 
   const listaTrilhas = [
-    {
-      nomeTrilha: "Trilha da Costa da Lagoa",
-      cidade: "Florianópolis",
-      estado: "SC",
-      duracao: "120",
-      trajeto: "4",
-      dificuldade: "iniciante",
-      tipo: "caminhada / trekking",
-      nomeUsuario: "Guilherme André",
-      urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    }
+
   ]
 
   return (
