f3616de28b3ab4b08e91aea4bb009124286dd318
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-04-22 01:53:00 -0300

    Exercicios semana 13

--- src/components/CardTrilha/index.jsx
+++ src/components/CardTrilha/index.jsx
@@ -1,28 +1,34 @@
 import * as PropTypes from "prop-types";
 import "./style.css";
 function CardTrilha({ dadosTrilha }) {
-
-    return (
-        <div className="card_container">
-            <img className="card_imagem" width={200} src={dadosTrilha.urlImagem} alt="imagem trilha" />
-            <h1>{dadosTrilha.nomeTrilha}</h1>
-            <span>{dadosTrilha.cidade} / {dadosTrilha.estado}</span>
-        </div>
-    );
+ return (
+  <div className="card_container">
+   <img
+    className="card_imagem"
+    width={200}
+    src={dadosTrilha.urlImagem}
+    alt="imagem trilha"
+   />
+   <h1>{dadosTrilha.nomeTrilha}</h1>
+   <span>
+    {dadosTrilha.cidade} / {dadosTrilha.estado}
+   </span>
+  </div>
+ );
 }
 
 CardTrilha.propTypes = {
-    dadosTrilha: PropTypes.exact({
-        nomeTrilha: PropTypes.string.isRequired,
-        cidade: PropTypes.string.isRequired,
-        estado: PropTypes.string.isRequired,
-        duracao: PropTypes.number.isRequired,
-        trajeto: PropTypes.number.isRequired,
-        dificuldade: PropTypes.string.isRequired,
-        tipo: PropTypes.oneOf(["caminhada / trekking", "ciclismo"]),
-        nomeUsuario: PropTypes.string.isRequired,
-        urlImagem: PropTypes.string.isRequired
-    })
-}
+ dadosTrilha: PropTypes.exact({
+  nomeTrilha: PropTypes.string.isRequired,
+  cidade: PropTypes.string.isRequired,
+  estado: PropTypes.string.isRequired,
+  duracao: PropTypes.number.isRequired,
+  trajeto: PropTypes.number.isRequired,
+  dificuldade: PropTypes.string.isRequired,
+  tipo: PropTypes.oneOf(["caminhada / trekking", "ciclismo"]),
+  nomeUsuario: PropTypes.string.isRequired,
+  urlImagem: PropTypes.string.isRequired
+ })
+};
 
 export default CardTrilha;
